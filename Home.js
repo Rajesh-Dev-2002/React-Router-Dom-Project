a22f2a509803914503cffecbbaf77edffd809d7f
Rajesh Kumar Mohanta <119045660+Rajesh7853814223@users.noreply.github.com>
2023-09-05 19:49:16 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import Header from './Components/Header'
+
+export default function Home() {
+  return (
+    <div>
+        <Header/>
+        <h1>Home Page Only</h1>
+      
+    </div>
+  )
+}
